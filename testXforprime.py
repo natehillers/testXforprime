e51d93ce3e80235709887081412dee3a04f46535
natehillers <nhillers@gmail.com>
2016-11-09 15:02:07 -0800

    added code

---
+++ testXforprime.py
@@ -0,0 +1,18 @@
+## Simple test whether inputed number is prime
+
+testprime = input("What number do you want to test for prime? ")
+
+def isprime(n):
+    n = abs(int(n))
+    if n < 2:
+        return False
+    if n == 2: 
+        return True
+    if not n & 1:
+        return False
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
+
+print(isprime(testprime))
